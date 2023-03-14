f66cf0aa71d472be2ec273c8f0e2b3f75676e3cb
José Luis <113980643+PePeLu04@users.noreply.github.com>
2023-03-14 09:34:37 +0100

    Update iControllerComp.java

--- src/main/java/proyectoGimnasia/interfaces/iControllerComp.java
+++ src/main/java/proyectoGimnasia/interfaces/iControllerComp.java
@@ -3,6 +3,9 @@ package proyectoGimnasia.interfaces;
 import proyectoGimnasia.model.Competicion;
 
 public interface iControllerComp {
+	void empezar();
+	void execMainMenu();
+	void controlMainMenu(int option);
 	void execRegistrarComp();
 	void registrarComp(Competicion c);
 	void execBorrarComp();
@@ -10,4 +13,10 @@ public interface iControllerComp {
 	void execUpdate();
 	void update(int index, Competicion c, Competicion aux);
 	void ListarComp();
+	void execRegistrarPrueba();
+	void registrarPrueba(int index, Prueba c);
+	void execRemovePrueba();
+	void removePrueba(int index, String nombre);
+	void execUpdatePrueba();
+	void updatePrueba(int index, String nombre);
 }
